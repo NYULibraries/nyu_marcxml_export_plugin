a20841cbeeae47be694b84f3e5c4f479379ec5f2
Esha Datta <ed64@nyu.edu>
2018-04-25 16:56:18 -0400

    added  local functionality for 600 and 610 subfields

--- backend/model/nyu_custom_model_marc21.rb
+++ backend/model/nyu_custom_model_marc21.rb
@@ -46,6 +46,142 @@ class MARCModel < ASpaceExport::ExportModel
     df('040', ' ', ' ').with_sfs(['a', repo['org_code']], ['b', langcode],['c', repo['org_code']])
   end
 
+  def handle_agents(linked_agents)
+
+    handle_primary_creator(linked_agents)
+
+    subjects = linked_agents.select{|a| a['role'] == 'subject'}
+
+    subjects.each_with_index do |link, i|
+      subject = link['_resolved']
+      name = subject['display_name']
+      relator = link['relator']
+      terms = link['terms']
+      ind2 = source_to_code(name['source'])
+
+      case subject['agent_type']
+
+      when 'agent_corporate_entity'
+        code = '610'
+        ind1 = '2'
+        sfs = [
+          ['a', name['primary_name']],
+          ['b', name['subordinate_name_1']],
+          ['b', name['subordinate_name_2']],
+          ['n', name['number']],
+          ['g', name['qualifier']],
+        ]
+
+      when 'agent_person'
+        joint, ind1 = name['name_order'] == 'direct' ? [' ', '0'] : [', ', '1']
+        name_parts = [name['primary_name'], name['rest_of_name']].reject{|i| i.nil? || i.empty?}.join(joint)
+        ind1 = name['name_order'] == 'direct' ? '0' : '1'
+        code = '600'
+        sfs = [
+          ['a', name_parts],
+          ['b', name['number']],
+          ['c', %w(prefix title suffix).map {|prt| name[prt]}.compact.join(', ')],
+          ['q', name['fuller_form']],
+          ['d', name['dates']],
+          ['g', name['qualifier']],
+        ]
+
+      when 'agent_family'
+        code = '600'
+        ind1 = '3'
+        sfs = [
+          ['a', name['family_name']],
+          ['c', name['prefix']],
+          ['d', name['dates']],
+          ['g', name['qualifier']],
+        ]
+
+      end
+
+      terms.each do |t|
+        tag = case t['term_type']
+        when 'uniform_title'; 't'
+        when 'genre_form', 'style_period'; 'v'
+        when 'topical', 'cultural_context'; 'x'
+        when 'temporal'; 'y'
+        when 'geographic'; 'z'
+        end
+        sfs << [(tag), t['term']]
+      end
+
+      if ind2 == '7'
+        create_sfs2 = %w(local ingest)
+        sfs << ['2', 'local'] if create_sfs2.include?(subject['display_name']['source'])
+      end
+
+      df(code, ind1, ind2, i).with_sfs(*sfs)
+    end
+
+
+    creators = linked_agents.select{|a| a['role'] == 'creator'}[1..-1] || []
+    creators = creators + linked_agents.select{|a| a['role'] == 'source'}
+
+    creators.each do |link|
+      creator = link['_resolved']
+      name = creator['display_name']
+      relator = link['relator']
+      terms = link['terms']
+      role = link['role']
+
+      if relator
+        relator_sf = ['4', relator]
+      elsif role == 'source'
+        relator_sf =  ['e', 'former owner']
+      else
+        relator_sf = ['e', 'creator']
+      end
+
+      ind2 = ' '
+
+      case creator['agent_type']
+
+      when 'agent_corporate_entity'
+        code = '710'
+        ind1 = '2'
+        sfs = [
+          ['a', name['primary_name']],
+          ['b', name['subordinate_name_1']],
+          ['b', name['subordinate_name_2']],
+          ['n', name['number']],
+          ['g', name['qualifier']],
+        ]
+
+      when 'agent_person'
+        joint, ind1 = name['name_order'] == 'direct' ? [' ', '0'] : [', ', '1']
+        name_parts = [name['primary_name'], name['rest_of_name']].reject{|i| i.nil? || i.empty?}.join(joint)
+        ind1 = name['name_order'] == 'direct' ? '0' : '1'
+        code = '700'
+        sfs = [
+          ['a', name_parts],
+          ['b', name['number']],
+          ['c', %w(prefix title suffix).map {|prt| name[prt]}.compact.join(', ')],
+          ['q', name['fuller_form']],
+          ['d', name['dates']],
+          ['g', name['qualifier']],
+        ]
+
+      when 'agent_family'
+        ind1 = '3'
+        code = '700'
+        sfs = [
+          ['a', name['family_name']],
+          ['c', name['prefix']],
+          ['d', name['dates']],
+          ['g', name['qualifier']],
+        ]
+      end
+
+      sfs << relator_sf
+      df(code, ind1, ind2).with_sfs(*sfs)
+    end
+
+  end
+
   def handle_dates(dates)
     return false if dates.empty?
 
@@ -73,53 +209,54 @@ class MARCModel < ASpaceExport::ExportModel
     notes.each do |note|
 
       prefix =  case note['type']
-                when 'dimensions'; "Dimensions"
-                when 'physdesc'; "Physical Description note"
-                when 'materialspec'; "Material Specific Details"
-                when 'physloc'; "Location of resource"
-                when 'phystech'; "Physical Characteristics / Technical Requirements"
-                when 'physfacet'; "Physical Facet"
-                when 'processinfo'; "Processing Information"
-                when 'separatedmaterial'; "Materials Separated from the Resource"
-                else; nil
-                end
+      when 'dimensions'; "Dimensions"
+      when 'physdesc'; "Physical Description note"
+      when 'materialspec'; "Material Specific Details"
+      when 'physloc'; "Location of resource"
+      when 'phystech'; "Physical Characteristics / Technical Requirements"
+      when 'physfacet'; "Physical Facet"
+      when 'processinfo'; "Processing Information"
+      else; nil
+      end
 
       marc_args = case note['type']
 
-                  when 'arrangement', 'fileplan'
-                    ['351','b']
-                  when 'odd', 'dimensions', 'physdesc', 'materialspec', 'physloc', 'phystech', 'physfacet', 'processinfo', 'separatedmaterial'
-                    ['500','a']
-                  when 'accessrestrict'
-                    ['506','a']
-                  when 'abstract'
-                    ['520', '3', ' ', 'a']
-                  when 'prefercite'
-                    ['524', '8', ' ', 'a']
-                  when 'acqinfo'
-                    ind1 = note['publish'] ? '1' : '0'
-                    ['541', ind1, ' ', 'a']
-                  when 'relatedmaterial'
-                    ['544','a']
-                  when 'custodhist'
-                    ind1 = note['publish'] ? '1' : '0'
-                    ['561', ind1, ' ', 'a']
-                  when 'appraisal'
-                    ind1 = note['publish'] ? '1' : '0'
-                    ['583', ind1, ' ', 'a']
-                  when 'accruals'
-                    ['584', 'a']
-                  when 'altformavail'
-                    ['535', '2', ' ', 'a']
-                  when 'originalsloc'
-                    ['535', '1', ' ', 'a']
-                  when 'userestrict', 'legalstatus'
-                    ['540', 'a']
-                  when 'langmaterial'
-                    ['546', 'a']
-                  else
-                    nil
-                  end
+      when 'arrangement', 'fileplan'
+        ['351','b']
+      when 'odd', 'dimensions', 'physdesc', 'materialspec', 'physloc', 'phystech', 'physfacet', 'processinfo'
+        ['500','a']
+      when 'accessrestrict'
+        ['506','a']
+      when 'abstract'
+        ['520', '3', ' ', 'a']
+      when 'prefercite'
+        ['524', '8', ' ', 'a']
+      when 'acqinfo'
+        ind1 = note['publish'] ? '1' : '0'
+        ['541', ind1, ' ', 'a']
+      when 'separatedmaterial'
+        ['544', '0', ' ', 'n']
+      when 'relatedmaterial'
+        ['544', '1', ' ', 'n']
+      when 'custodhist'
+        ind1 = note['publish'] ? '1' : '0'
+        ['561', ind1, ' ', 'a']
+      when 'appraisal'
+        ind1 = note['publish'] ? '1' : '0'
+        ['583', ind1, ' ', 'a']
+      when 'accruals'
+        ['584', 'a']
+      when 'altformavail'
+        ['535', '2', ' ', 'a']
+      when 'originalsloc'
+        ['535', '1', ' ', 'a']
+      when 'userestrict', 'legalstatus'
+        ['540', 'a']
+      when 'langmaterial'
+        ['546', 'a']
+      else
+        nil
+      end
 
       unless marc_args.nil?
         text = prefix ? "#{prefix}: " : ""

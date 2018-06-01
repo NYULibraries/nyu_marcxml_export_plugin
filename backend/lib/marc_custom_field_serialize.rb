ad973ef02e5a791ba0b86a92b78e52fd5fce7333
Donald Mennerich <don.mennerich@nyu.edu>
2018-06-01 15:09:50 -0400

    appending offsite to calncy cullen  tag

--- backend/lib/marc_custom_field_serialize.rb
+++ backend/lib/marc_custom_field_serialize.rb
@@ -249,7 +249,7 @@ class MARCCustomFieldSerialize
 
   def location_hsh
     {
-      "Clancy Cullen" => "DM",
+      "Clancy Cullen [Offsite]" => "DM",
       "20 Cooper Square [Offsite Prep]" => "OK",
       "Bobst [Offsite Prep]" => "OP"
     }
